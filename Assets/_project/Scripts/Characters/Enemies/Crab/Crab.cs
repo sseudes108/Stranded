74e07e08f46a7c76e535f420665b33d59aeead0a
Eudes <senstur.art@gmail.com>
2024-06-21 09:26:26 -0300

    Complete patrol logic. Start Chase Logic

--- Assets/_project/Scripts/Characters/Enemies/Crab/Crab.cs
+++ Assets/_project/Scripts/Characters/Enemies/Crab/Crab.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class Crab : MachineController{
     public CrabPatrol CrabPatrol;
     public IdleState Idle => StateMachine.IdleState;
-    public RunState Run => StateMachine.RunState;
+    public RunState Chase => StateMachine.RunState;
     public JumpState Jump => StateMachine.JumpState;
     public PatrolPoint[] PatrolPoints;
+
+    [SerializeField] private Transform _aggroCheckBox;
+    [SerializeField] private Vector2 _aggroCheckSize;
+    public Player Player;
+
+    public readonly int MOVE = Animator.StringToHash("Crab_Move");
+    public readonly int IDLE = Animator.StringToHash("Crab_Idle");
 
     public override void Awake() {
         base.Awake();
@@ -28,5 +35,18 @@ public class Crab : MachineController{
             Run = new CrabRun(),
             Jump = new CrabJump(),
         });
+    }
+
+    public bool CheckTarget(){
+        Collider2D chase = Physics2D.OverlapBox(_aggroCheckBox.position, _aggroCheckSize, 0, LayerMask.GetMask("Player"));
+        if(chase){
+            Debug.Log("Chase");
+        }
+        return chase;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(_aggroCheckBox.position,_aggroCheckSize);
     }
 }
--- Assets/_project/Scripts/Characters/Enemies/Crab/CrabStates.cs
+++ Assets/_project/Scripts/Characters/Enemies/Crab/CrabStates.cs
@@ -2,14 +2,64 @@ using System.Collections;
 using UnityEngine;
 
 public class CrabPatrol : Abstract{
+    private int _currentPoint;
+    private float _wait;
+    private float _direction;
     public override void EnterState(){
 
     }
 
     public override void LogicUpdate(){
-
+        CheckTarget();
+        Patrol();
+        FlipSpriteDirection();
     }
 
+    private void FlipSpriteDirection(){
+        if (_direction == 1){
+            Crab.transform.localScale = new Vector3(-1, 1, 1);
+        }if (_direction == -1){
+            Crab.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private void Patrol(){
+        if (Vector3.Distance(Crab.transform.position, Crab.PatrolPoints[_currentPoint].transform.position) > 0.2f){
+            _wait = 5f;
+            if (Crab.Movement.Body.transform.position.x > Crab.PatrolPoints[_currentPoint].transform.position.x){
+                Crab.Movement.SetDirection(-1f);
+                _direction = -1f;
+            }else{
+                Crab.Movement.SetDirection(1f);
+                _direction = 1f;
+            }
+            if (Crab.Animation.CurrentAnimation == Crab.MOVE){
+                return;
+            }
+            Crab.ChangeAnimation(Crab.MOVE);
+        }else{
+            Crab.Movement.SetDirection(0);
+            _wait -= Time.deltaTime;
+            if (_wait <= 0){
+                _currentPoint++;
+                if (_currentPoint >= Crab.PatrolPoints.Length)
+                {
+                    _currentPoint = 0;
+                    _wait = 0;
+                }
+            }
+            if (Crab.Animation.CurrentAnimation == Crab.IDLE){
+                return;
+            }
+            Crab.ChangeAnimation(Crab.IDLE);
+        }
+    }
+
+    private void CheckTarget(){
+        if (Crab.CheckTarget()){
+            Crab.ChangeState(Crab.Chase);
+        }
+    }
     public override void PhysicsUpdate(){
         
     }
@@ -41,11 +91,17 @@ public class CrabIdle : IdleState{
 
 //Chase
 public class CrabRun : RunState{
+    private Player _target;
     public override void EnterState(){
-
+        Debug.Log("Chase State");
+        _target = Crab.FindFirstObjectByType<Player>();
     }
     public override void LogicUpdate(){
+        if (Vector3.Distance(Crab.transform.position, _target.transform.position) > 0.2f){
+            Vector3.MoveTowards(Crab.transform.position, _target.transform.position, Crab.Movement.GetSpeed());
+        }else{
 
+        }
     }
     public override void PhysicsUpdate(){
 
